6bebdc3565054491ab1f20e251f38122fc7b2b37
rhett <rhettapples@googleemailservice.com>
2024-12-02 13:48:31 -0600

    qcom

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+
+type person struct {
+	firstName string
+	lastName string
+	born int
+	died int
+	bc_ad string
+}
+
+
+func main() {
+
+
+	Tutankhamun := person{firstName: "Tutankhamun", lastName: "", born: 1341, died: 1323, bc_ad: "BC"}	
+	// figure out siddartha here
+	Socrates := person{firstName: "Socrates", lastName: "", born: 470, died: 399, bc_ad: "BC"}
+	// figure out plato's exact birthdate, or put error bars
+	Alexander := person{firstName: "Alexander", lastName: "the Great", born: 356, died: 323, bc_ad: "BC"}
+
+	Archimedes := person{firstName: "Archimedes", lastName: "of Syracuse", born: 287, died: 212, bc_ad: "BC"}
+
+	JuliusCaesar := person{firstName: "Julius", lastName: "Caesar", born: 100, died: 44, bc_ad: "BC"}
+
+	GenghisKhan := person{firstName: "Genghis", lastName: "Khan", born: 1162, died: 1227, bc_ad: "AD"}
+
+	
+
+
+	
+	people := []person{Tutankhamun, Socrates, Alexander, Archimedes, JuliusCaesar, GenghisKhan}
+	
+
+	for i := 0; i < len(people); i++ {
+		fmt.Println(people[i])
+	}
+
+}
